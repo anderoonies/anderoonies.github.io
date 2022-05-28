a6d435d85f805d4a2aa9363c43817a1f1bff60ec
Andy Bayer <andy@panic.com>
2022-05-28 15:31:10 -0700

    overflow fixy

--- _includes/halftone/stilllife.js
+++ _includes/halftone/stilllife.js
@@ -10,10 +10,14 @@ let STILL_LIFE_WIDTH, STILL_LIFE_HEIGHT;
 const stillLifeReady = new Promise((resolve) => {
   stillLife.src = "/projects/halftone/stilllife.jpeg";
   stillLife.onload = () => {
+    const aspectRatio = stillLife.height / stillLife.width;
+    stillLife.width = Math.min(stillLife.width, window.screen.width - 50);
+    stillLife.height = stillLife.width * aspectRatio;
+
     STILL_LIFE_WIDTH = stillLife.width;
     STILL_LIFE_HEIGHT = stillLife.height;
     resize(stillLifeCanvas, STILL_LIFE_WIDTH, STILL_LIFE_HEIGHT);
-    stillLifeCtx.drawImage(stillLife, 0, 0);
+    stillLifeCtx.drawImage(stillLife, 0, 0, STILL_LIFE_WIDTH, STILL_LIFE_HEIGHT);
     resolve();
   };
 });
